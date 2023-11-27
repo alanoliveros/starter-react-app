87e0230115cdab2aa0769aac99bcf2107238c068
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-11-28 01:47:41 +0800

    unsaon mani

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js YWAAA?</code> and save to reload.
+          Edit <code>src/App.js SURE UY</code> and save to reload.
         </p>
         <a
           className="App-link"
