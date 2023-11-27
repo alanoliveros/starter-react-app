16243f8f2554a67baaf291193f6d33f213060356
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-11-28 01:52:11 +0800

    sure jud

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js SURE UY</code> and save to reload.
+          SURE <code>UY</code>!
         </p>
         <a
           className="App-link"
