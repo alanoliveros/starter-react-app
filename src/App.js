8311d15c3cdd722bc0c30aeeae72642d0af7ed36
Alan Supla-ag Oliveros <71147809+alanoliveros@users.noreply.github.com>
2023-11-28 02:04:21 +0800

    gi remove

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ function App() {
         <p>
           SURE <code>UY</code> .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
     </div>
   );
